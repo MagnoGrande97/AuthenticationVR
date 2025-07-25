71751a47e2bad453e945808f39be4d19f2ffd238
Magno Grande <magno1997@hotmail.com>
2025-07-25 09:25:08 -0500

    Removed

--- SOLID/DTOs/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DragerBackendTemplate.SOLID.DTOs
-{
-    public class LoginRequest
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
--- SOLID/Models/MongoDbSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DragerBackendTemplate.SOLID.Models
-{
-    public class MongoDbSettings
-    {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-        public string UsersCollectionName { get; set; }
-    }
-}
