71e6f03f2734ca41cc1f8fede8a1eb37fa53b6ae
Magno Grande <magno1997@hotmail.com>
2025-08-07 12:43:47 -0500

    Update usuario.model.js

--- usuario.model.js
+++ usuario.model.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const usuarioSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   email: { type: String, required: true },
-  uID: { type: String, required: true, unique: true }
+  uID: { type: String, required: true, unique: true },
+  telefono: { type: String, required: false },  // asegúrate que no sea required
+  rol: { type: String, required: false }        // lo mismo aquí
 });
 
 module.exports = mongoose.model('Usuario', usuarioSchema);
